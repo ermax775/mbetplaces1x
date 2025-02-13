9b388ddb395d72665b86ad1dc5ab344f6c455792
ermax775 <ermax7715@gmail.com>
2025-02-13 12:50:11 +0300

    Marker "You" stopped functioning

--- App.js
+++ App.js
@@ -5,12 +5,19 @@ import TabNavigation from './App/Navigation/TabNavigation';
 import { useEffect, useState } from 'react';
 import * as Location from 'expo-location'; 
 import { UserLocationContext } from './App/Context/UserLocationContext';
+import { useFonts } from 'expo-font';
 
 export default function App() {
 
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
+  const [fontsLoaded] = useFonts({
+    'raleway': require('./assets/Fonts/Raleway-Regular.ttf'),
+    'raleway-bold': require('./assets/Fonts/Raleway-SemiBold.ttf'),
+
+  });
+   
 
 
   useEffect (() => {
--- App/Components/GoogleMapView.js
+++ App/Components/GoogleMapView.js
@@ -1,13 +1,37 @@
 import { View, Text, Dimensions } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 // import MapView from 'react-native-maps'
-import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { MapMarker, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { UserLocationContext } from '../Context/UserLocationContext';
 
 
 export default function GoogleMapView() {
+
+  const [mapRegion, setmapRegion] = useState([]);
+
+  const { location, setLocation } = useContext(UserLocationContext);
+
+  useEffect(()=>{
+    if(location)
+    {
+        setmapRegion({
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0422,
+            longitudeDelta: 0.0421,
+        })
+    }
+  },[location])
+ 
+
   return (
     <View style={{ borderRadius: 20, overflow: "hidden" }}>
-      {/* <Text> GoogleMapView </Text> */}
+
+      <Text style={{ fontSize: 20, 
+        marginBottom: 10, fontWeight: "600",fontFamily:'raleway-bold' }}>
+        MBet-Adera's Near By Places
+      </Text>      
+      
       <MapView 
         style={{
           width: Dimensions.get("screen").width * 0.89,
@@ -17,6 +41,19 @@ export default function GoogleMapView() {
       showsUserLocation={true}
 
       >
+        {/* <Marker 
+        
+          title="You" 
+          coordinate={mapRegion}
+
+        /> */}
+
+      {/* <MapMarker 
+        title="You" 
+        coordinate={mapRegion}
+      /> */}
+
+
 
       </MapView>
     </View>
